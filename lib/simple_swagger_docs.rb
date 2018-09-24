6585c45453f344d26f0d6e3ffa9019f47b72ffa8
yukihirop <te108186@gmail.com>
2018-09-24 13:32:41 +0900

    Update SimpleSwaggerDocs

--- lib/simple_swagger_docs.rb
+++ lib/simple_swagger_docs.rb
@@ -1,4 +1,6 @@
 require "simple_swagger_docs/version"
+require_relative "simple_swagger_docs/v2/schema/generated/swagger_object"
+require_relative "simple_swagger_docs/v3/schema/generated/openapi_object"
 
 module SimpleSwaggerDocs
   # Your code goes here...
